f176777c84119dc97a1a3e1870862498d914379f
Carol Martin <carolmartin@cox.net>
2018-07-04 22:37:19 -0700

    initial code

---
+++ assets/javascript/app.js
@@ -0,0 +1,219 @@
+// Trivia GAME Logic
+// =============================
+
+// This code will run as soon as the page loads
+window.onload = function () {
+  $("#start").on("click", trivia.start);
+  $("#stop").on("click", trivia.stop);
+
+};
+
+//  Variable that will hold our setInterval that runs the timer
+var intervalId;
+// const myQuestions = [
+//   {
+//     question: "Who is the strongest?",
+//     answers: {
+//       a: "Superman",
+//       b: "The Terminator",
+//       c: "Waluigi, obviously"
+//     },
+//     correctAnswer: "c"
+//   },
+//   {
+//     question: "What is the best site ever created?",
+//     answers: {
+//       a: "SitePoint",
+//       b: "Simple Steps Code",
+//       c: "Trick question; they're both the best"
+//     },
+//     correctAnswer: "c"
+//   },
+const MyQuestions = [
+  {
+    question: "What are words that sound the same but have different meanings?",
+    answers: {
+      a: "Homophones",
+      b: "Superlatives",
+      c: "Endonyms"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Which one of the following is the best conductor of electicity?",
+    answers: {
+      a: "Silver",
+      b: "Rubber",
+      c: "Wood"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "When a number is halfway between two numbers, how do you round?",
+    answers: {
+      a: "down",
+      b: "up",
+      c: "either up or down"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "The war of 1812 was fought between the United States and what country?",
+    answers: {
+      a: "France",
+      b: "Germany",
+      c: "England"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Name the chemical that makes plant leaves green?",
+    answers: {
+      a: "Chlorophyl",
+      b: "Oxygen",
+      c: "Hydrogen"
+    },
+    correctAnswer: "a"
+  }
+] 
+
+
+// prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+
+// Our stopwatch object
+var stopwatch = {
+
+  time: 0,
+  lap: 1,
+
+  reset: function () {
+
+    stopwatch.time = 0;
+    stopwatch.lap = 1;
+
+    // DONE: Change the "display" div to "00:00."
+    $("#display").text("00:00");
+
+    // DONE: Empty the "laps" div.
+    $("#laps").text("");
+  },
+  start: function () {
+
+    // DONE: Use setInterval to start the count here and set the clock to running.
+    if (!clockRunning) {
+      intervalId = setInterval(stopwatch.count, 1000);
+      clockRunning = true;
+    }
+  },
+  stop: function () {
+
+    // DONE: Use clearInterval to stop the count here and set the clock to not be running.
+    clearInterval(intervalId);
+    clockRunning = false;
+  },
+  recordLap: function () {
+
+    // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+    //       and save the result in a variable.
+    var converted = stopwatch.timeConverter(stopwatch.time);
+
+    // DONE: Add the current lap and time to the "laps" div.
+    $("#laps").append("<p>Lap " + stopwatch.lap + " : " + converted + "</p>");
+
+    // DONE: Increment lap by 1. Remember, we can't use "this" here.
+    stopwatch.lap++;
+  },
+  count: function () {
+
+    // DONE: increment time by 1, remember we cant use "this" here.
+    stopwatch.time++;
+
+    // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
+    //       and save the result in a variable.
+    var converted = stopwatch.timeConverter(stopwatch.time);
+    console.log(converted);
+
+    // DONE: Use the variable we just created to show the converted time in the "display" div.
+    $("#display").text(converted);
+  },
+  timeConverter: function (t) {
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
+};
+
+
+// Solution if you choose not to put it in an object
+
+// var time = 0;
+// var lap = 1;
+// function reset() {
+
+//   time = 0;
+//   lap = 1;
+
+//   $("#display").text("00:00");
+//   $("#laps").text("");
+
+// }
+
+// function start() {
+//   intervalId = setInterval(count, 1000);
+// }
+
+// function stop() {
+
+//   console.log("stopping");
+//   clearInterval(intervalId);
+
+// }
+
+// function recordLap() {
+
+//   var converted = timeConverter(time);
+//   $("#laps").append("<p>Lap " + lap + " : " + converted + "</p>");
+//   lap++;
+
+// }
+
+// function count() {
+
+//   time++;
+//   var converted = timeConverter(time);
+//   $("#display").text(converted);
+
+// }
+
+// function timeConverter(t) {
+
+//   var minutes = Math.floor(t / 60);
+//   var seconds = t - (minutes * 60);
+
+//   if (seconds < 10) {
+//     seconds = "0" + seconds;
+//   }
+
+//   if (minutes === 0) {
+//     minutes = "00";
+//   }
+//   else if (minutes < 10) {
+//     minutes = "0" + minutes;
+//   }
+
+//   return minutes + ":" + seconds;
+// }
